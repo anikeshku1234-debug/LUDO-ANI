e5f62d108d399a2b75690952ce670fa641818a89
anikeshku1234-debug <anikeshku1234@gmail.com>
2026-09-23 06:05:27 +0530

    Update server.js

--- LUDO ANI/server.js
+++ LUDO ANI/server.js
@@ -8,7 +8,7 @@ const server = http.createServer(app);
 const io = new Server(server, { cors: { origin: "*" } });
 
 const PORT = process.env.PORT || 10000;
-
+app.use(express.static(path.join(__dirname)));
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
